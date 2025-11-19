8775224cac8cdd84031d90bc2bb5c318378a73d4
rachana <rachanakethapelly0709@gmail.com>
2025-11-19 21:57:35 +0530

    push

---
+++ src/ex_01_Java_Basics/program002_printnextline.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class program002_printnextline
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello,");
+        System.out.print(" I");
+        System.out.print(" AM");
+        System.out.print(" RACHANA");
+
+    }
+}
