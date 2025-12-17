9fe7bef6f68727e77aa775cfee7abfc6871d30c3
rachana <rachanakethapelly0709@gmail.com>
2025-12-17 21:28:36 +0530

    push

---
+++ src/ex_01_Java_Basics/program003_datatypes.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basics;
+
+import java.util.Arrays;
+
+public class program003_datatypes
+{
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 10;
+        int i= 10;
+        char c = 'a';
+        //for long "l" should be added in the end, it can be capital or small leeter. The same applies for float.
+        long l = 9959349147L;
+        float f = 3.13f;
+        double d = 3.133333333;
+        System.out.println(f);
+    }
+}
