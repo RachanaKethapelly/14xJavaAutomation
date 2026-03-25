2eebd3675992b92066d5e535a58e26aacefe880d
rachana <rachanakethapelly0709@gmail.com>
2026-03-25 22:25:19 +0530

    push

---
+++ src/ex_01_Java_Basics/program018_functions.java
@@ -0,0 +1,32 @@
+package ex_01_Java_Basics;
+
+public class program018_functions
+{
+    public static void main(String[] args)
+    {
+        /* \t is tab space
+           \b is backspace
+           \n is newline
+           \r is carriage return
+           \' single  coute character
+           \" double coute character
+           \\backslash character
+
+         */
+
+        // math function , all mathematical functions can be performed
+
+        int max = Math.max(5,7);
+        System.out.println(max);
+        int min = Math.min(3,9);
+        System.out.println(min);
+       int multiply =  Math.multiplyExact(4,7);
+        System.out.println(multiply);
+
+        //here we have 4 types of methods, with parameters, without parameters, with return type and without return type :
+
+
+    }
+}
+
+
